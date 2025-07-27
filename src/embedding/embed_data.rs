5599bc3cb60f7c176b14fddd140d15b73abd44b2
pmrch <fejespatrik2007@gmail.com>
2025-07-27 19:27:27 +0200

    feat: FInished embedding implementation

--- src/embedding/embed_data.rs
+++ src/embedding/embed_data.rs
@@ -1,11 +1,36 @@
+use core::fmt;
+use std::str::FromStr;
+use serde_json;
+
 use crate::prelude::*;
 use super::*;
 
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Debug)]
+pub struct ParseEmbeddingDataError(String);
+
+impl fmt::Display for ParseEmbeddingDataError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Failed to parse EmbeddingData: {}", self.0)
+    }
+}
+
+impl std::error::Error for ParseEmbeddingDataError {}
+
+
+#[derive(Serialize, Deserialize, Clone)]
 pub struct EmbeddingData {
     object: String,
     data: EmbeddingResponse,
     model: String,
     usage: EmbeddingUsage
+}
+
+impl FromStr for EmbeddingData {
+    type Err = ParseEmbeddingDataError;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        serde_json::from_str(s)
+            .map_err(|e| ParseEmbeddingDataError(e.to_string()))
+    }
 }
