d0013cab80849c461360f8c6e948d4b00368315a
pmrch <fejespatrik2007@gmail.com>
2025-07-27 20:09:03 +0200

    fix: Fix the final bug in embeddings

--- src/embedding/embed_data.rs
+++ src/embedding/embed_data.rs
@@ -20,10 +20,10 @@ impl std::error::Error for ParseEmbeddingDataError {}
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct EmbeddingData {
-    object: String,
-    data: EmbeddingResponse,
-    model: String,
-    usage: EmbeddingUsage
+    pub object: String,
+    pub data: Vec<EmbeddingResponse>,
+    pub model: String,
+    pub usage: EmbeddingUsage
 }
 
 impl FromStr for EmbeddingData {
@@ -32,5 +32,14 @@ impl FromStr for EmbeddingData {
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
         serde_json::from_str(s)
             .map_err(|e| ParseEmbeddingDataError(e.to_string()))
+    }
+}
+
+impl fmt::Display for EmbeddingData {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match serde_json::to_string_pretty(self) {
+            Ok(s) => write!(f, "{}", s),
+            Err(e) => write!(f, "<failed to display EmbeddingData: {}>", e)
+        }
     }
 }
